b8a1d71118e0190ae4014293ff0d211a12562352
vinycxuz <vinyby@gmail.com>
2025-03-05 15:28:16 -0300

    feat: created 3 routes for courses /titles /authors /all

---
+++ Course/routes.py
@@ -0,0 +1,26 @@
+from Flask import Blueprint, jsonify
+from . import db
+from .models import Course
+
+routes = Blueprint('routes', __name__)
+
+
+@routes.route('/courses/title', methods=['GET'])
+def get_titles():
+    courses = Course.query.all()
+    titles = [courses.title for course in courses]
+    return jsonify({"titles": title})
+
+
+@routes.route('/courses/authors', methods=['GET'])
+def get_authors():
+    courses = Course.query.all()
+    authors = [course.author for course in courses]
+    return jsonify({"authors": authors})
+
+@routes.route('/', methods=['GET'])
+def get_courses():
+    courses = Course.query.all()
+    list = [course for course in courses]
+    return jsonify({"courses": list})
+
